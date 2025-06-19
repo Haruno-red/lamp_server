50001682406a156593854fb58a16e55a678194bc
Haruno-red <haruno.kobayashi1126@gmail.com>
2025-06-19 03:33:57 +0000

    update test.sh

--- lamp_compose/test.sh
+++ lamp_compose/test.sh
@@ -1,18 +1,46 @@
-#!/bin/bash
+#!/bin/bash -eu
 
 # 環境変数が設定されているか確認
-if [ -z "$MYSQL_ROOT_PASSWORD" ] || [ -z "$MYSQL_USER" ] || [ -z "$MYSQL_PASSWORD" ] || [ -z "$MYSQL_DATABASE" ];
-then
-  echo "Error: 環境変数 MYSQL_ROOT_PASSWORD, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE が設定されていません。"
-  exit 1
-fi
+function check_env() {
+  output=""
+  success="Succeeded: 環境変数が設定されています。"
+  if [ -z "$MYSQL_ROOT_PASSWORD" ]; then
+    output="${output}Error: 環境変数 MYSQL_ROOT_PASSWORD が設定されていません。\n"
+  fi
+  if [ -z "$MYSQL_USER" ]; then
+    output="${output}Error: 環境変数 MYSQL_USER が設定されていません。\n"
+  fi
+  if [ -z "$MYSQL_PASSWORD" ]; then
+    output="${output}Error: 環境変数 MYSQL_PASSWORD が設定されていません。\n"
+  fi
+  if [ -z "$MYSQL_DATABASE" ]; then
+    output="${output}Error: 環境変数 MYSQL_DATABASE が設定されていません。\n"
+  fi
+  if [ -z "${output}" ]; then
+    output="$success"
+    echo "$output"
+  else
+    echo -e "$output"
+    exit 1    
+  fi
+  return 0
+}
 
-# MySQLに接続する
-mysql --user=root --password=$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\`;"
-if [ $? -eq 0 ];
-then
-  echo "データベース '$MYSQL_DATABASE' が正常に作成されました。"
-else
-  echo "データベース '$MYSQL_DATABASE' の作成に失敗しました。"
-fi
+
+# MySQLに接続して、auth_testというデータベースを作成
+function create_database(){
+  mysql --user=root --password=$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\`;"
+  if [ $? -eq 0 ]; then
+    echo "データベース '$MYSQL_DATABASE' が正常に作成されました。"
+  else
+    echo "データベース '$MYSQL_DATABASE' の作成に失敗しました。"
+  fi
+}
+
+function main(){
+  check_env
+  create_database
+}
+
+main
 
