5dd3f486460866fbd91b3119bf179af282bbdd4e
Haruno-red <haruno.kobayashi1126@gmail.com>
2025-06-20 03:00:26 +0000

    update lamp_compose/test.sh

--- lamp_compose/test.sh
+++ lamp_compose/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eu
 
 # 環境変数が設定されているか確認
-function check_env() {
+function check_env(){
   local error_message=""
   if [ -z "$MYSQL_ROOT_PASSWORD" ]; then
     error_message="${error_message}Error: 環境変数 MYSQL_ROOT_PASSWORD が設定されていません。\n"
@@ -25,9 +25,26 @@ function check_env() {
   fi
 }
 
+# MySQL実行関数（第一引数：データベース名、第二引数：SQL文）
+function exec_mysql(){
+  local db=""
+  local query=""
+  if [ "$#" -eq 1 ]; then
+    query="$1"
+  elif [ "$#" -eq 2 ]; then
+    db="-D $1"
+    query="$2"
+  else
+    echo "引数の数が正しくありません。"
+    return 1
+  fi
+  mysql --user=root --password=$MYSQL_ROOT_PASSWORD $db -e "$query"
+  return 0
+}
+
 # auth_testというデータベースを作成
 function create_database(){
-  mysql --user=root --password=$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\`;"
+  exec_mysql "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\`;"
   if [ $? -eq 0 ]; then
     echo "データベース '$MYSQL_DATABASE' が正常に作成されました。"
     return 0
@@ -39,10 +56,10 @@ function create_database(){
 
 # auth_testデータベース内にtestテーブルを作成し、サンプルデータを格納
 function setup_table(){
-  mysql --user=root --password=$MYSQL_ROOT_PASSWORD -D "$MYSQL_DATABASE" -e "CREATE TABLE IF NOT EXISTS test (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(20), age INT);"
+  exec_mysql "$MYSQL_DATABASE" "CREATE TABLE IF NOT EXISTS test (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(20), age INT);"
   if [ $? -eq 0 ]; then
     echo "テーブル 'test' が正常に作成されました。"
-    mysql --user=root --password=$MYSQL_ROOT_PASSWORD -D "$MYSQL_DATABASE" -e "INSERT INTO test (name, age) VALUES ('abc', '25'), ('def', '26'), ('ghi', '27');"
+    exec_mysql "$MYSQL_DATABASE" "INSERT INTO test (name, age) VALUES ('abc', '25'), ('def', '26'), ('ghi', '27');"
     if [ $? -eq 0 ]; then
       echo "テーブル 'test' のデータが更新されました。"
       return 0
@@ -58,8 +75,15 @@ function setup_table(){
 
 # 外部からの接続を許可したsample_userというユーザを作成
 function create_user(){
-  mysql --user=root --password=$MYSQL_ROOT_PASSWORD -e "CREATE USER 'sample_user' identified by '$MYSQL_PASSWORD';"
-  mysql --user=root --password=$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON auth_test.test TO 'sample_user'@'%' WITH GRANT OPTION;"
+  exec_mysql "CREATE USER '$MYSQL_USER' identified by '$MYSQL_PASSWORD';"
+  exec_mysql "GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.test TO '$MYSQL_USER'@'%' WITH GRANT OPTION;"
+  if [ $? -eq 0 ]; then
+    echo "ユーザ '$MYSQL_USER' が正常に作成されました。"
+    return 0
+  else
+    echo "ユーザ '$MYSQL_USER' の作成に失敗しました。"
+    return 1
+  fi
 }
 
 
